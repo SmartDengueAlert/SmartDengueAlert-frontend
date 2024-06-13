49f7087f603d442c75555f6d4013749856267e52
DilshanaRanawake <dilshanaranawake@gmail.com>
2024-06-13 12:36:51 +0530

    adding prediction.dart file

---
+++ lib/prediction.dart
@@ -0,0 +1,14 @@
+import 'dart:convert';
+// import 'package:http/http.dart' as http;
+
+// Future<String> getPrediction(String location) async {
+//   final response = await http.get(
+//     Uri.parse('http://localhost:5000/predict?location=$location'),
+//   );
+//   if (response.statusCode == 200) {
+//     return jsonDecode(response.body)['message'];
+//   } else {
+//     print('Failed to get prediction');
+//     return '';
+//   }
+// }
